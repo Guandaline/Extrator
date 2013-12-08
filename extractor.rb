3a9b71247d16f74ee9fd640d471ad310b69345d5
guandaline <vhguandaline@gmail.com>
2013-12-08 03:31:36 -0200

    Verificando se um estrutua e simples ou composta

--- extractor.rb
+++ extractor.rb
@@ -4,8 +4,9 @@ require 'nokogiri/diff'
 require 'pry'
 
 def runHtml(node, list)
-  	p = node.parent;
+  	p = node.parent
   	path = p.path.gsub(/\[[0-9]+\]/, '')
+  	#path = p.path
 	if list.has_key?(path)
 		num = list.fetch(path)
 		num += 1
@@ -46,10 +47,70 @@ def printPath(doc1, path)
 	doc1.xpath(path).each do |node|
 		p = node.parent()
 		parent = p.parent()
-		puts node.path
-		puts node.name + ' ' + parent.to_html + "\n\n"
+		puts parent.path
+		puts node.to_html + ' ' + "\n\n"
 	end
 end
+
+def extruct(doc1, path)
+	doc1.xpath(path)[0..10].each do |node|
+		puts node.path
+		puts 'Numero de filhos de ' + node.name  + ' ' + node.children.length.to_s
+		if  node.children.length > 1 
+			if typeChildrenHeterogeneos?(node)
+				puts 'Informação em: ' + node.path
+			else
+				puts 'Menu em: ' + node.path;
+			end
+		else 
+			if !node.child.element?()
+				puts 'Não é um elemento = ' + node.child.content  
+			else 
+				if typeChildrenHeterogeneos?(node.)
+					puts 'Informação em: ' + node.path
+				else
+					puts 'Menu em: ' + node.path;
+				end
+			end
+		end
+		typeParent(node.parent)
+	end
+end
+
+def typeParent(node)
+	if node.children == 1
+		typeParent(node.parent)
+	else 
+		if typeChildrenHeterogeneos?(node)
+			puts 'Informação em: ' + node.path
+		else
+			puts 'Menu em: ' + node.path;
+		end
+	end
+
+end
+
+def typeChildrenHeterogeneos?(node) 
+	list = Hash.new 
+	node.children.each do |children|
+		key = children.name
+		if list.has_key?(key)
+			num = list.fetch(key)
+			num += 1
+			list[key] = num 
+		else
+			list[key] = 1
+		end
+	end
+	
+	if list.size > 1
+		return true
+	end
+	return false
+end
+
+
+
 #doc1 = Nokogiri::HTML(open('http://www.pontofrio.com.br/'))
 doc1 = Nokogiri::HTML(open('http://www.magazineluiza.com.br/'))
 #doc1 = Nokogiri::HTML(open('http://www.nytimes.com/'))
@@ -60,13 +121,13 @@ doc1 = Nokogiri::HTML(open('http://www.magazineluiza.com.br/'))
 list = getMostFreqPath(doc1)
 
 list.each do |key, val|
-	doc1.xpath(key).each do |node|
-		if node.element?
-			puts key + ' é um elemento ' + node.children.length(). to_s
-		else
-			puts key + ' é um text'
-		end
-	end
+	#doc1.xpath(key).each do |node|
+#		if node.element?
+#			puts key + ' é um elemento ' + node.children.length(). to_s
+#		else
+#			puts key + ' é um text'
+#		end
+#	end
 	puts '' + key.to_s + ' ' + val.to_s 
 end
 
@@ -76,4 +137,5 @@ path  = first.first
 
 puts doc1.xpath(path).size
 
-#printPath(doc1, path)
+#printPath(doc1, '/html/body/div/div/div/div/div/a')
+extruct(doc1, path)
